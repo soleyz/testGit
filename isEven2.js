2a6869e4fdc058ff858a6bcf39f86b9ba6269ec6
soleyz <jack@Jacks-MacBook-Pro.local>
2018-08-19 12:26:16 +0700

    first commit for tesst2

---
+++ isEven2.js
@@ -0,0 +1,13 @@
+function isEven(number){
+    if (number == 1){
+        return false
+    }
+    else if (number ==0){
+        return true
+    }
+    else if (number != 0){
+        return(isEven(number-2))
+    }
+}
+console.log(isEven(50));
+console.log(isEven(75));
