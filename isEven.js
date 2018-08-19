2f0fbc8ecb4b6296177acd73169c3eddd7618ee5
soleyz <jack@Jacks-MacBook-Pro.local>
2018-08-19 21:29:10 +0700

    add some line

--- isEven.js
+++ isEven.js
@@ -6,8 +6,10 @@ function isEven(number){
         return true
     }
     else if (number != 0){
+        console.log("Recursion :" +number);
         return(isEven(number-2))
     }
 }
+console.log(isEven(25));
 console.log(isEven(50));
 console.log(isEven(75));
